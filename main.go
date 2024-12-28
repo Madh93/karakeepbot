f4a023673f51aefb22fe51497447f23014b197a4
Madh93 <mhdez@protonmail.com>
2024-12-28 15:59:11 +0000

    ci: setup release workflow

---
+++ main.go
@@ -0,0 +1,13 @@
+// Package main is the entry point of the application. It initializes the
+// configuration, sets up logging, and starts the Hoarderbot.
+package main
+
+import (
+	"fmt"
+)
+
+// main initializes the configuration, sets up logging, and starts the
+// Hoarderbot.
+func main() {
+	fmt.Println("Hello World!")
+}
