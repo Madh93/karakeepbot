7b483e68c7182092d2e10473e28fbf46562d93f5
Madh93 <mhdez@protonmail.com>
2025-08-14 17:31:18 +0200

    feat: enhance Telegram with file URL retrieval and photo sending

--- internal/karakeepbot/telegram.go
+++ internal/karakeepbot/telegram.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/Madh93/karakeepbot/internal/config"
 	"github.com/Madh93/karakeepbot/internal/logging"
+	"github.com/Madh93/karakeepbot/internal/secret"
 	tgbotapi "github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 )
 
 // Bot is an alias for tgbotapi.Bot.
@@ -15,6 +17,7 @@ type Bot = tgbotapi.Bot
 // Telegram embeds the Telegram bot API client to add high level functionality.
 type Telegram struct {
 	*Bot
+	token secret.String
 }
 
 // createTelegram initializes the Telegram Bot API client.
@@ -26,7 +29,7 @@ func createTelegram(logger *logging.Logger, config *config.TelegramConfig) *Tele
 		logger.Fatal("Error creating Telegram Bot API.", "error", err)
 	}
 
-	return &Telegram{telegramBot}
+	return &Telegram{Bot: telegramBot, token: config.Token}
 }
 
 // SendNewMessage sends a new message to the user's chat.
@@ -35,6 +38,38 @@ func (t Telegram) SendNewMessage(ctx context.Context, msg *TelegramMessage) erro
 		ChatID:          msg.Chat.ID,
 		MessageThreadID: msg.MessageThreadID,
 		Text:            msg.Text,
+	}
+
+	if _, err := t.SendMessage(ctx, params); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// SendPhotoWithCaption sends a photo with a caption.
+func (t *Telegram) SendPhotoWithCaption(ctx context.Context, msg *TelegramMessage, photoID string, caption string) error {
+	params := &tgbotapi.SendPhotoParams{
+		ChatID:          msg.Chat.ID,
+		MessageThreadID: msg.MessageThreadID,
+		Photo:           &models.InputFileString{Data: photoID},
+		Caption:         caption,
+	}
+
+	if _, err := t.SendPhoto(ctx, params); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// SendReply sends a reply to a specific message.
+func (t Telegram) SendReply(ctx context.Context, msg *TelegramMessage, text string) error {
+	params := &tgbotapi.SendMessageParams{
+		ChatID:          msg.Chat.ID,
+		MessageThreadID: msg.MessageThreadID,
+		ReplyParameters: &models.ReplyParameters{MessageID: msg.ID},
+		Text:            text,
 	}
 
 	if _, err := t.SendMessage(ctx, params); err != nil {
@@ -56,4 +91,13 @@ func (t Telegram) DeleteOriginalMessage(ctx context.Context, msg *TelegramMessag
 	}
 
 	return nil
+}
+
+// GetFileURL returns the download URL for a given file ID.
+func (t Telegram) GetFileURL(ctx context.Context, fileID string) (string, error) {
+	file, err := t.GetFile(ctx, &tgbotapi.GetFileParams{FileID: fileID})
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("https://api.telegram.org/file/bot%s/%s", t.token.Value(), file.FilePath), nil
 }
